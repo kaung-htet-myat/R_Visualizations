a9e634f6660b6de1efe75807f4775ab88c08b502
Christopher-K-Constantine <kaunghm.09@gmail.com>
2019-03-14 22:05:08 +0630

    R Visualization Update

--- R_Visual_1.R
+++ R_Visual_1.R
@@ -1,9 +1,27 @@
 library(ggplot2)
 data("midwest",package="ggplot2")
 
-ggplot(midwest, aes(x=area,y=poptotal)) +
-  geom_point() +
-  geom_smooth(method="lm") +
-  #xlim(c(0,0.1)) +
-  #ylim(c(0,1000000))
-  coord_cartesian(xlim(c(0,0.1)), ylim(c(0,1000000)))
+# Simple Plotting
+g <- ggplot(midwest, aes(x=area,y=poptotal)) +
+      geom_point(aes(col=state),size=3) +
+      geom_smooth(method="lm")
+      #xlim(c(0,0.1)) +
+      # ylim(c(0,1000000))
+  
+g1 <- g +
+      coord_cartesian(xlim=c(0,0.1), ylim=c(0,1000000)) +
+      labs(title="Area Vs Population", subtitle="From midwest dataset", y="Population", x="Area", caption="Midwest Demographics")
+
+plot(g1)
+
+# Boxplotting
+
+df <- iris
+par(mfrow=c(1,4))
+  for(i in 1:4){
+    boxplot(df[,i],main=names(iris)[i])
+  }
+
+# Histogram
+par(mfrow=c(1,1))
+hist(iris$Sepal.Length, col="steelblue",main = "Length of the sepal" ,xlab="Length of the sepal")
